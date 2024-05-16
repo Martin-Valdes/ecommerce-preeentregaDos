8005069054110c313a6c10199810b5031a6c18b3
Fernando Valdes <140764694+Martin-Valdes@users.noreply.github.com>
2024-05-16 13:57:33 -0300

    Resuelto conflicto después de hacer pull

--- src/router/carts.routes.js
+++ src/router/carts.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { randomUUID } from 'crypto';
+
 
 const router = Router();
 
 
-const newId = randomUUID();
+
 
 
 export default router;
--- src/router/products.routes.js
+++ src/router/products.routes.js
@@ -1,21 +1,99 @@
 import { Router } from "express";
+import { randomUUID } from 'crypto';
+
 
 const router = Router();
 
-let products = [];
-
-
-router.get("/", (req, res) =>{
+class productManager {
     
-    res.status(200).json(products);
-})
+    constructor(){
+        this.products = [];
+    }
+    
+    addProduct(product){
 
+        const {title, description, code, price, status, stock, category, thumbnails} = product;
+      
+        const newProduct = {
+            id: randomUUID(),
+            title,
+            description,
+            code,
+            price,
+            status,
+            stock,
+            category,
+            thumbnails
+        };
+        
+        const verifyRepit = this.products.find(product => product.code === code);
+
+        if (verifyRepit) throw new Error("Ya existe un producto con este codigo");
+
+        if(Object.values(newProduct).includes(undefined))throw new Error("Debes completar todos los campos");
+
+        this.products.push( newProduct);
+       
+    }
+    getProducts() {
+
+        return this.products
+    }
+
+    getProductById({id}){
+
+        const product = this.products.find( product => product.id === id)
+        return product
+
+    }
+    putProduct({id}){
+        let productIndex = this.products.index( product => product.id === id)
+        /// falta definir put me da error en este metodo
+      
+    }
+
+}
+
+const productNew = new productManager();
+
+//respuesta a la solicitud de lista completa de productos
+const productList = productNew.getProducts()
+
+router.get("/",(req, res) =>{
+   
+    res.status(200).json({productList});
+});
+
+//FUNCION PARA RETORNAR EL PRODUCTO POR ID MEDIANTE REQ.PARAMS
+router.get("/:id",(req, res) =>{
+   
+    const {id} = req.params;
+    
+    const productId = productNew.getProductById({id})
+    res.send(productId)
+
+
+});
+
+//fUNCION PARA ALMACENAR UN NUEVO PRODUCTO
 router.post ("/", (req, res) =>{
 
     const product = req.body;
-    products.push(product);
 
-    res.status(201).json({status: "status", msg: "Porducto almacenado"})
+    productNew.addProduct(product);
+
+    res.status(201).json({status: "status", msg: "Porducto almacenado"});
 })
+
+router.put("/:id",(req, res) =>{
+   
+    let dataProduct = req.body;
+    const {id} = req.params;
+    
+    const productId = productNew.putProduct({id})
+    res.send(productId)
+
+
+});
 
 export default router;
